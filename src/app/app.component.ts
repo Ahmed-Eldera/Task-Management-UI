30b17a05fd975adfebe0c9e7b74ae28342a15a2c
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-08-22 21:40:25 +0300

    intial card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { TaskCardComponent } from "./components/task-card/task-card.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, TaskCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
